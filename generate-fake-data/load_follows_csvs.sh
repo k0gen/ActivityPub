10f4f92697e45ea9c9483a3023d0f28ccfbe0a28
Daniel Lockyer <hi@daniellockyer.com>
2024-12-02 14:11:32 +0100

    Fixed calculating filename

--- generate-fake-data/load_follows_csvs.sh
+++ generate-fake-data/load_follows_csvs.sh
@@ -7,16 +7,18 @@ FOLLOWERS_OR_FOLLOWINGS=$1
 docker exec -it scale-testing mysql -uroot -proot $DATABASE -e "SET FOREIGN_KEY_CHECKS=0;"
 
 for file in $(ls output/$FOLLOWERS_OR_FOLLOWINGS*); do
-    echo "Importing $file into follows"
+    f="$(basename -- $file)"
+
+    echo "Importing $f into follows"
     time docker exec -it scale-testing mysql -uroot -proot -e "
-    LOAD DATA INFILE '/var/lib/mysql-files/csvs/$file'
+    LOAD DATA INFILE '/var/lib/mysql-files/csvs/$f'
     INTO TABLE $DATABASE.follows
     FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
     LINES TERMINATED BY '\n'
     IGNORE 1 ROWS
     (follower_id, following_id);
     "
-    echo "Imported $file into follows"
+    echo "Imported $f into follows"
 done
 
 docker exec -it scale-testing mysql -uroot -proot $DATABASE -e "SET FOREIGN_KEY_CHECKS=1;"
