34b8962b3e95010f525a24ecfdbbced0e2c1a39c
Daniel Lockyer <hi@daniellockyer.com>
2024-12-09 17:15:54 +0100

    Switched to pigz for compression
    - this is way faster than gzip but still produces gzip data

--- generate-fake-data-js-csv/data-export.sh
+++ generate-fake-data-js-csv/data-export.sh
@@ -8,6 +8,6 @@ for file in ./data/*.csv; do
     if [ -f "$file" ]; then
         echo "Compressing $file..."
 
-        gzip -9 -c "$file" > "./data/gz/$(basename "$file").gz"
+        pigz -c "$file" > "./data/gz/$(basename "$file").gz"
     fi
 done
